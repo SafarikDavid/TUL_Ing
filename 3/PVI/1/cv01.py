afe32655814bee84e08e3f71a4e1bea64e966c0d
David Safarik <d.safarik21@gmail.com>
2023-09-24 20:23:14 +0200

    PVI cv 1 done

--- 3/PVI/1/cv01.py
+++ 3/PVI/1/cv01.py
@@ -23,14 +23,12 @@ if __name__ == "__main__":
     # cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
     # segment car in image
-    only_car = rgb.copy()
-    only_car[seg <= 0] = 0
-    # cv2.imshow("ss", only_car)
-    # cv2.waitKey()
+    rgb_only_car = rgb.copy()
+    rgb_only_car[seg <= 0] = 0
 
-    pixels = only_car.reshape(-1, 3)
+    pixels = rgb_only_car.reshape(-1, 3)
     pixels = pixels[np.any(pixels != [0, 0, 0], axis=-1)]
-    kmeans = KMeans(n_clusters=1)
+    kmeans = KMeans(n_clusters=2)
     kmeans.fit(pixels)
 
     counts = np.bincount(kmeans.labels_)
@@ -41,16 +39,25 @@ if __name__ == "__main__":
 
     # bounding rectangle
     x, y, w, h = cv2.boundingRect(contours[0])
-    img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    img = cv2.rectangle(img, (x, y), (x + w, y + h), (int(dominant_color[2]), int(dominant_color[1]), int(dominant_color[0])), 3)
 
     # spz
     # https://www.youtube.com/watch?v=n-8oCPjpEvM
+    gray_only_car = gray.copy()
+    gray_only_car[seg <= 0] = 0
+    gray_only_car = gray_only_car[y:y + h, x:x + w]
+
     reader = easyocr.Reader(['cs'], gpu=False)
-    text = reader.readtext(gray[x:x+w, y:y+h])
+    text = reader.readtext(gray_only_car)
 
     for t in text:
-        print(t[1])
+        bbox, text, score = t
+        # text_again = reader.readtext(gray_only_car[int(bbox[0][1]):int(bbox[2][1]), int(bbox[0][0]):int(bbox[2][0])])
+        text_origin = [int(x) for x in bbox[0]]
+        text_origin = [text_origin[0] + x, text_origin[1] + y]
+        cv2.putText(img, text, text_origin, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        print(text)
 
     # show car
-    cv2.imshow("center of car", img)
+    cv2.imshow("car", img)
     cv2.waitKey()
