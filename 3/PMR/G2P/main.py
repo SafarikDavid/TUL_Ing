44a6fe4a30db1d4350a282c41760d7bff6516b51
David Safarik <d.safarik21@gmail.com>
2023-09-25 12:26:48 +0200

    PMR g2p

--- 3/PMR/G2P/main.py
+++ 3/PMR/G2P/main.py
@@ -21,14 +21,15 @@ def apply_rule_old(text, str_from, str_to, condition):
 
 def phonetic_rule(rule):
     split_rule = re.split("/", rule)
-    split_left_side = re.split("→", re.sub(" ", "", split_rule[0]))
+    split_left_side = re.split("->", re.sub(" ", "", split_rule[0]))
     condition = re.sub(" ", "", split_rule[1])
     str_from = split_left_side[0]
     str_to = split_left_side[1]
     return str_from, str_to, condition
 
 
-def apply_rule(text, str_from, str_to, condition):
+def apply_rule(text, rule):
+    str_from, str_to, condition = phonetic_rule(rule)
     condition = re.sub(r"-", " ", condition)
     condition = re.sub(r",", "", condition)
     condition = re.sub(r"<", "[", condition)
@@ -37,16 +38,21 @@ def apply_rule(text, str_from, str_to, condition):
     condition_left = re.split(r"_", condition)[0]
     condition_right = re.split(r"_", condition)[1]
 
-    sub_pattern = r"("+condition_left+")"+str_from+"("+condition_right+")"
+    sub_pattern = r"(" + condition_left + ")" + str_from + "(" + condition_right + ")"
 
-    return re.sub(sub_pattern, "\\1"+str_to+"\\2", text)
+    return re.sub(sub_pattern, "\\1" + str_to + "\\2", text)
 
 
 def main():
-    rule = "ch → X / <a,e,i,o,u>_"
-    text = "chichi chi ch i ch ich"
-    from_regex, to_regex, condition = phonetic_rule(rule)
-    print(apply_rule(text, from_regex, to_regex, condition))
+    rules = []
+    with open('pravidla.txt', 'r', encoding="utf-8") as file:
+        while line := file.readline():
+            if len(line.rstrip()) > 0:
+                rules.append(line.rstrip())
+    text = "chaluha ůpě wolf qantum vykalený výklad běhá pěvec vědec dědic tětiva někdy měnič díra titěra nikdy"
+    for rule in rules:
+        text = apply_rule(text, rule)
+    print(text)
 
 
 if __name__ == '__main__':
