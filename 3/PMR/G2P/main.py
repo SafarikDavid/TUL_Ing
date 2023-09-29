0dcfcc4aa0674af992fb55e8f8f2ba9eb5641077
David Safarik <d.safarik21@gmail.com>
2023-09-29 17:43:53 +0200

    PMR

--- 3/PMR/G2P/main.py
+++ 3/PMR/G2P/main.py
@@ -1,6 +1,5 @@
 import re
 
-
 spodoba_znelosti = {}
 
 
@@ -31,16 +30,34 @@ def apply_rule(text, rule):
     condition_left = re.split(r"_", condition)[0]
     condition_right = re.split(r"_", condition)[1]
 
-    sub_pattern = r"(" + condition_left + ")" + str_from + "(" + condition_right + ")"
-
     if str_to == "A":
-        return text
+        # copy of text
+        out = text[:]
+        for letter in str_from:
+            sub_pattern = r"(" + condition_left + ")" + letter + "(" + condition_right + ")"
+            letter_change = spodoba_znelosti.get(letter)
+            if letter_change is None: continue
+            out = re.sub(sub_pattern, "\\1" + letter_change + "\\2", out)
+        return out
+
+    sub_pattern = r"(" + condition_left + ")" + str_from + "(" + condition_right + ")"
 
     return re.sub(sub_pattern, "\\1" + str_to + "\\2", text)
 
 
 def main():
-    text = "chaluha ůpě wolf qantum vykalený výklad běhá pěvec vědec dědic tětiva někdy měnič díra titěra nikdy"
+    # text = ("chaluha ůpě wolf qantum vykalený výklad běhá pěvec vědec dědic tětiva někdy měnič díra titěra nikdy kresba pětset xavier marie mariji biologie hrad vůz Radka drozd"
+    #         " kresba kdo leckde keř břicho tři přímo pařba \n gong banka tramvaj nymfa triumf amfóra emvej \n "
+    #         "pět set větší beskydský mladší podzemí ledzkde kamikadze džordž lodžije džungle")
+    # text = 'Spolek byl založen devatenáctého listopadu roku devatenáct set třicet dva'
+    # text = 'Sejdeme se v naší restauraci ve čtvrt na sedm večer'
+    # text = 'Kdy dnes odjíždí poslední vlak nebo autobus z Liberce do Pardubic'
+    # text = 'Na konferenci senátor rovněž kritizoval současné právní prostředí'
+    # text = 'Výkon brankáře znamenal pro hokejové družstvo dobré umístění v tabulce'
+    # text = 'Dnes bude oblačno až polojasno, místy možno očekávat přeháňky'
+    # text = 'Najdeš to ve zdrojovém kódu HTML, stačí hledat řetězec mp3'
+
+    short_sentences = {}
 
     spodoba_znelosti_raw = ''
     substitutes = {}
@@ -66,12 +83,39 @@ def main():
     for sub_raw in substitutes_raw:
         left, right = sub_raw.split(":")
         substitutes[left] = right
-    for rule in rules:
-        for key in substitutes.keys():
-            rule = re.sub(key, substitutes[key], rule)
-        text = apply_rule(text, rule)
-    text = re.sub(' ', '_', text)
-    print(text)
+
+    transcription = []
+    text_arr = []
+    with open('text.txt', 'r', encoding='utf-8') as file:
+        while line := file.readline():
+            text_arr.append(line)
+    sentence_idx = 1
+    for line in text_arr:
+        text = line[:]
+        text = text.strip()
+        text = text.lower()
+        text = ' ' + text + ' '
+        text = re.sub('[,.]', '', text)
+
+        if len(re. findall(r'\w+', text)) < 10:
+            short_sentences[sentence_idx] = text
+        sentence_idx += 1
+
+        for rule in rules:
+            for key in substitutes.keys():
+                rule = re.sub(key, substitutes[key], rule)
+            text = apply_rule(text, rule)
+        text = re.sub(' ', '_', text)
+        text = text.strip()
+        text = text.strip('_')
+        text = '-' + text + '-'
+        transcription.append(text)
+        print(text)
+    with open('transcription.txt', 'w', encoding='utf-8') as file:
+        for line in transcription:
+            file.write(line)
+            file.write('\n')
+    print(short_sentences)
 
 
 if __name__ == '__main__':
