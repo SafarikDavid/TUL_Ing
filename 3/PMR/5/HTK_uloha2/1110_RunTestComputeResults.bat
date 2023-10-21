18cae79b75e3e59d7d990ec2913b70fd33d4e6cc
David Safarik <d.safarik21@gmail.com>
2023-10-22 00:46:41 +0200

    PMR

--- 3/PMR/5/HTK_uloha2/1110_RunTestComputeResults.bat
+++ 3/PMR/5/HTK_uloha2/1110_RunTestComputeResults.bat
@@ -1,3 +1,3 @@
-HVite -H mono-32_8/hmmdefs -S test.scp -i recout.mlf -w wordnet -p -70.0 -s 1 dict models0
+HVite -H mono-32_8/hmmdefs -S test.scp -i recout.mlf -w wordnet -p -130.0 -s 1 dict models0
 HResults -e ??? SENT-START -e ??? SENT-END -t -I testref.mlf models0 recout.mlf
 pause
