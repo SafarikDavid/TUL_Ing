09d052746c4cb4552e075ed671eb96d5fb7d5b5f
David Safarik <d.safarik21@gmail.com>
2023-11-01 23:39:55 +0100

    PMR

--- 3/PMR/6/HTK_Quarter_train/12_runtestandcomputeresluts.bat
+++ 3/PMR/6/HTK_Quarter_train/12_runtestandcomputeresluts.bat
@@ -1,3 +1,3 @@
-HVite -H mono-2_4/hmmdefs -S test.scp -i recout.mlf -w wordnet -p -70.0 -s 0 dict models0
+HVite -H mono-16_2/hmmdefs -S test.scp -i recout.mlf -w wordnet -p -70.0 -s 0 dict models0
 HResults -t -I testref.mlf models0 recout.mlf
 pause
