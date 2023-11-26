24ecbeb06544a9616a215f6d83cc1f0d29ee0cfb
David Safarik <d.safarik21@gmail.com>
2023-11-26 20:53:34 +0100

    PVI

--- 3/PVI/9/cv09.py
+++ 3/PVI/9/cv09.py
@@ -11,7 +11,7 @@ def main():
     MIN_MATCH_COUNT = 10
     img1 = cv.imread('data/obcansky_prukaz_cr_sablona_2012_2014.png')  # queryImage
     img1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
-    img2 = cv.imread('data/TS10_01.jpg')  # trainImage
+    img2 = cv.imread('data/HS10_12.jpg')  # trainImage
     img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
     img2_copy = img2.copy()
 
@@ -72,16 +72,30 @@ def main():
     plt.imshow(warped, 'gray')
     plt.show()
 
-    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-
-    surname_cutout = (warped[78:110, 150:327]*255).astype(np.uint8)
-    first_name_cutout = (warped[105:135, 150:327]*255).astype(np.float32)
+    surname_cutout = (warped[78:110, 150:350]*255).astype(np.uint8)
+    first_name_cutout = (warped[105:135, 150:350]*255).astype(np.float32)
     face_cutout = (warped[136:402, 17:233]*255).astype(np.float32)
 
     surname_text = reader.readtext(surname_cutout, detail=0)
     first_name_text = reader.readtext(first_name_cutout, detail=0)
     print(surname_text[0])
     print(first_name_text[0])
+
+    face_with_text = face_cutout.copy()
+    face_with_text = cv.putText(img=face_with_text,
+                      text=first_name_text[0],
+                      org=(10, 15),
+                      fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                      fontScale=0.4,
+                      color=(0, 0, 255),
+                      thickness=1)
+    face_with_text = cv.putText(img=face_with_text,
+                      text=surname_text[0],
+                      org=(10, 30),
+                      fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                      fontScale=0.4,
+                      color=(0, 0, 255),
+                      thickness=1)
 
     plt.subplot(3, 1, 1)
     plt.imshow(surname_cutout, 'gray')
@@ -90,6 +104,9 @@ def main():
     plt.subplot(3, 1, 3)
     plt.imshow(face_cutout, 'gray')
 
+    plt.show()
+
+    plt.imshow(face_with_text, 'gray')
     plt.show()
 
 
